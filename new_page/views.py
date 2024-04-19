c6da124b8a5cd61d5114ec7f656d6585c9b9bc92
milksut <altaygokis@gmail.com>
2024-04-19 14:59:51 +0300

    database tests
    Signed-off-by: milksut <altaygokis@gmail.com>

--- new_page/views.py
+++ new_page/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 
 class HomePageView(TemplateView):
     template_name = "homepage.html"
 
 
-class SecondPageView(TemplateView):
+class SecondPageView(ListView):
+    model = Post
     template_name = "second_page.html"
