72ee69e4a1aaf7b5dd295998cab12955b229a1a9
Melik Yalcinkaya <melikyalcinkaya55gmail.com>
2024-05-20 15:22:21 +0300

    fixes

--- new_page/admin.py
+++ new_page/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post,Match
 
 admin.site.register(Post)
+
+from .models import Match
+
+admin.site.register(Match)
--- new_page/models.py
+++ new_page/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
+# models.py
+from django.db import models
+
+class Match(models.Model):
+    team1 = models.CharField(max_length=100)
+    team2 = models.CharField(max_length=100)
+    match_date = models.DateField()
+    odds_team1 = models.FloatField()
+    odds_draw = models.FloatField()
+    odds_team2 = models.FloatField()
+
+    def __str__(self):
+        return f"{self.team1} vs {self.team2} - {self.match_date}"
+
+
 
 class Post(models.Model):
     text = models.TextField()
