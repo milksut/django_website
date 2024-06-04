99a9c10a3ed0dcf699866c3c6a4ea17571db1a39
milksut <altaygokis@gmail.com>
2024-06-04 14:28:47 +0300

    kuponlraim-page backend bitti, css yok

--- new_page/views.py
+++ new_page/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.http import HttpRequest, JsonResponse, HttpResponse
 from django.utils.translation import gettext_lazy as tercuman
-import csv, io
+import csv, io, json
 from django.contrib.auth.decorators import  permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from new_page.models import Coach, Match, Kupon, Post
@@ -297,6 +297,19 @@ def KuponCall(request: HttpRequest):
         else:
             messages.error(request, 'Giriş yapmadan kupon oynayamassın!')
             return redirect('homepage')
+
+    elif request.method == "GET":
+        if request.user.is_authenticated:
+            kuponlar = Kupon.objects.filter(Kullanici=request.user)
+            oynanan_maclar={}
+            for kupon in kuponlar:
+                oynanan_maclar[kupon.id] = json.loads(kupon.Match) 
+            return render(request, 'kuponlarım_page.html', {'kuponlar':kuponlar, 'oynanan_maclar':oynanan_maclar})
+
+        else:
+            messages.error(request, 'Giriş yapmadan kuponları görüntüleyemessin!')
+            return redirect('homepage')
+
     else:
         messages.error(request, 'Geçersiz Method')
         return redirect('homepage')
